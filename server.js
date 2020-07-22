3abf0f904926f13990175cae561e29c2d090590a
Anthony Renwick <renwickman@gmail.com>
2020-07-22 10:04:28 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
+
+let multer = require('multer');
+
+app.post('/api/fileanalyse', multer().single('upfile'), (request, response) =>{
+  console.log(request.file)
+  let resObject = {}
+  resObject['name'] = request.file.originalname
+  resObject['type'] = request.file.mimetype
+  resObject['size'] = request.file.size
+
+  response.json({resObject})
+})
